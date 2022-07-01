538d44645a47af5362c1c6a3c063f935fc053469
guilherme daniel <gui.dani13@gmail.com>
2022-06-30 21:18:57 -0300

    add navigation drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,6 @@ import { DrawerRoutes } from "./drawer.routes";
 export function Routes() {
   return (
     <NavigationContainer>
-      {/* <StackRoutes /> */}
-      {/* <TabRoutes /> */}
       <DrawerRoutes />
     </NavigationContainer>
   );
