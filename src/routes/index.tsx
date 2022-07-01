e1fdd5383e0812fb7957b3c7f976604ff53b0cca
guilherme daniel <gui.dani13@gmail.com>
2022-06-30 21:15:04 -0300

    add stack navigation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { DrawerRoutes } from "./drawer.routes";
+import { StackRoutes } from "./stack.routes";
 
 export function Routes() {
   return (
     <NavigationContainer>
-      {/* <StackRoutes /> */}
-      {/* <TabRoutes /> */}
-      <DrawerRoutes />
+      <StackRoutes />
     </NavigationContainer>
   );
 }
